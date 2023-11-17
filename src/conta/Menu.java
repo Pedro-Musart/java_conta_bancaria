ab8037e059020eb1fabca01226dec2632a3acdd0
Pedro Soares <pedrowesleyfs@gmail.com>
2023-11-17 16:22:58 -0300

    implementei a classe contas com seus métodos

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,12 +1,19 @@
 package conta;
 
 import java.util.Scanner;
-
+import conta.model.Conta;
 import conta.util.Cores;
 
 public class Menu {
     public static void main(String[] args) {
-        
+    	
+    	Conta c1 = new Conta(1,123,1,"Pedro",1000.0f);
+    	c1.visualizar();
+    	c1.sacar(12000.0f);
+    	c1.visualizar();
+    	c1.depositar(5000.0f);
+    	c1.visualizar();
+    	
         Scanner leia = new Scanner(System.in);
 		
 		int opcao;
