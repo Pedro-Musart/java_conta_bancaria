b356e7c6848177943d078b9dcd05ae6d1d5720e9
Pedro Soares <pedrowesleyfs@gmail.com>
2023-11-23 16:07:09 -0300

    atualização

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -13,16 +13,6 @@ public class Menu {
 	
     public static void main(String[] args) {
      	
-
-    	
-    	// Teste da Classe Conta
-    	Conta c1 = new Conta(1,123,1,"Pedro",1000.0f);
-    	c1.visualizar();
-    	c1.sacar(12000.0f);
-    	c1.visualizar();
-    	c1.depositar(5000.0f);
-    	c1.visualizar();
-    	
     	// Teste da Classe Conta Corrente
     	ContaCorrente cc1 = new ContaCorrente(2,123,1,"Mariana",15000.0f, 1000.0f);
     	cc1.visualizar();
