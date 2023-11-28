fc25a0fab7ba97611bc0800c65bd9a1186a948a5
Pedro Soares <pedrowesleyfs@gmail.com>
2023-11-28 18:51:59 -0300

    finalizado o projeto

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -19,11 +19,12 @@ public class Menu {
      	  	
         Scanner leia = new Scanner(System.in);
 		
-        int opcao, numero, agencia, tipo, aniversario;
+        int opcao, numero, agencia, tipo, aniversario, numeroDestino;
         String titular;
-        float saldo, limite;
+        float saldo, limite, valor;
         
 		
+        
 				
 		while(true) {
 
@@ -95,9 +96,7 @@ public class Menu {
 						aniversario = leia.nextInt();
 						contas.cadastrar(new ContaPoupanca(contas.gerarNumero(), agencia, tipo, titular, saldo, aniversario));
 					}
-					
-					}
-					
+					}			
 					keyPress();
                     		break;
 				case 2:
@@ -107,32 +106,107 @@ public class Menu {
                     		break;
 				case 3:
 					System.out.println(Cores.TEXT_WHITE + "Consultar dados da Conta - por número\n\n");
+					numero = leia.nextInt();
+					
+					contas.procurarPorNumero(numero);
 					
 					keyPress();
                     		break;
 				case 4:
 					System.out.println(Cores.TEXT_WHITE + "Atualizar dados da Conta\n\n");
 					
+					System.out.println("Digite o número da Conta: ");
+					numero = leia.nextInt();
+					
+					var buscaConta = contas.buscarNaCollection(numero);
+					
+					if(buscaConta != null) {
+						tipo = buscaConta.getTipo();
+						
+						System.out.println("Digite o Numero da Agência: ");
+						agencia =  leia.nextInt();
+						System.out.println("Digite o Nome do Titular: ");
+						leia.skip("\\R?");
+						titular =  leia.nextLine();
+						
+						System.out.println("Digite o Saldo da Conta (R$):");
+						saldo = leia.nextFloat();
+						
+						switch (tipo) {
+						case 1 -> {
+							System.out.println("Digite o Limite de Crédito (R$): ");
+							limite = leia.nextFloat();
+							
+							contas.atualizar(new ContaCorrente(numero, agencia, tipo, titular, saldo,limite));
+							
+						}
+						case 2-> {
+							System.out.println("Digite o dia do Aniversario da Conta:");
+							aniversario = leia.nextInt();
+							
+							contas.atualizar(new ContaPoupanca(numero, agencia, tipo, titular, saldo, aniversario));
+						}
+						default ->{
+							System.out.println("Tipo de conta inválido!");
+						}
+						}
+					}else {
+						System.out.println("A conta não foi encontrada!");
+					}
+				
 					keyPress();
                     		break;
 				case 5:
 					System.out.println(Cores.TEXT_WHITE + "Apagar a Conta\n\n");
-		
+					System.out.println("Digite o numero da conta: ");
+					numero = leia.nextInt();
+					contas.deletar(numero);
+					
 					keyPress();
                    	 	break;
 				case 6:
 					System.out.println(Cores.TEXT_WHITE + "Saque\n\n");
-
+					System.out.println("Digite o numero da conta: ");
+					numero = leia.nextInt();
+					
+					do {
+						System.out.println("Digite o Valor do Saque(R$): ");
+						valor = leia.nextInt();
+						
+					}while( valor<= 0);
+					
+					contas.sacar(numero, valor);
 					keyPress();
                    	 	break;
 				case 7:
 					System.out.println(Cores.TEXT_WHITE + "Depósito\n\n");
+					System.out.println("Digite o numero da conta: ");
+					numero = leia.nextInt();
 					
+					do {
+						System.out.println("Digite o Valor do Depósito(R$): ");
+						valor = leia.nextInt();
+						
+					}while( valor<= 0);
+					
+					contas.depositar(numero, valor);
 					keyPress();
                     		break;
 				case 8:
 					System.out.println(Cores.TEXT_WHITE + "Transferência entre Contas\n\n");
+					System.out.println(Cores.TEXT_WHITE + "Depósito\n\n");
+					System.out.println("Digite o numero da conta Origem: ");
+					numero = leia.nextInt();
+					System.out.println("Digite o numero da conta Destino: ");
+					numeroDestino = leia.nextInt();
 					
+					do {
+						System.out.println("Digite o Valor da Tranferência(R$): ");
+						valor = leia.nextFloat();
+						
+					}while( valor<= 0);
+					
+					contas.transferir(numero,numeroDestino, valor);
 					keyPress();
                     		break;
 				default:
--- src/conta/controller/ContaController.java
+++ src/conta/controller/ContaController.java
@@ -14,8 +14,14 @@ public class ContaController implements ContaRepository {
 	
 	@Override
 	public void procurarPorNumero(int numero) {
-		// TODO Auto-generated method stub
+		var conta = buscarNaCollection(numero);
 		
+		if (conta != null) {
+			conta.visualizar();
+		}
+		else {
+			System.out.println("\nA Conta número: " + numero + "não foi encontrado!");
+		}
 	}
 
 	@Override
@@ -35,31 +41,70 @@ public class ContaController implements ContaRepository {
 
 	@Override
 	public void atualizar(Conta conta) {
-		// TODO Auto-generated method stub
+		var buscaConta = buscarNaCollection(conta.getNumero());
+		
+		if(buscaConta != null) {
+			listaContas.set(listaContas.indexOf(buscaConta), conta);
+			System.out.println("\nA Conta numero: " +  conta.getNumero() + "foi atualizada com sucesso!");
+		} else
+			System.out.println("\nA Conta numero: " + conta.getNumero() + "não foi  encontrada!");
 		
 	}
 
 	@Override
 	public void deletar(int numero) {
-		// TODO Auto-generated method stub
+		var conta = buscarNaCollection(numero);
+		
+		if(conta != null) {
+			if (listaContas.remove(conta) == true)
+				System.out.println("\nA Conta numero: " + numero + " foi deletada com sucesso!");
+		} else {
+			System.out.println("\nA Conta numero:"+ numero + " não foi encontrada!");
+		}
 		
 	}
 
 	@Override
 	public void sacar(int numero, float valor) {
-		// TODO Auto-generated method stub
+		var conta =  buscarNaCollection(numero);
+		
+		if (conta != null ) {
+			if(conta.sacar(valor) == true) {
+				
+				System.out.println("\nO Saque na Conta numero: " + numero + " foi efetuado com sucesso!");
+			}else {
+				System.out.println("\nA Conta numero: " + numero + " não foi encontrada!");
+			}
+		}
 		
 	}
 
 	@Override
 	public void depositar(int numero, float valor) {
-		// TODO Auto-generated method stub
+		var conta =  buscarNaCollection(numero);
 		
-	}
+		if (conta != null ) {
+			conta.depositar(valor);
+				System.out.println("\nDepósito na Conta numero: " + numero + " foi efetuado com sucesso!");
+			}else {
+				System.out.println("\nA Conta numero: " + numero + " não foi encontrada ou a Conta destino não é Conta Corrente!");
+			}
+		}
 
 	@Override
 	public void transferir(int numeroOrigem, int numeroDestino, float valor) {
-		// TODO Auto-generated method stub
+		var contaOrigem =  buscarNaCollection(numeroOrigem);
+		var contaDestino =  buscarNaCollection(numeroDestino);
+		
+		if (contaOrigem != null && contaDestino != null) {
+			
+		}
+				if(contaOrigem.sacar(valor) == true) {
+					contaDestino.depositar(valor);
+				System.out.println("\nA transferencia foi efetuada com sucesso!");
+			}else {
+				System.out.println("\nA Conta  Origem e/ou Destino não foram encontradas!");
+			}
 		
 	}
 	
@@ -67,5 +112,12 @@ public class ContaController implements ContaRepository {
 		return ++ numero;
 	}
 
-	
+	public Conta buscarNaCollection(int numero) {
+		for (var conta : listaContas) {
+			if (conta.getNumero() == numero) {
+				return conta;
+			}
+		}
+		return null;
+	}
 }
